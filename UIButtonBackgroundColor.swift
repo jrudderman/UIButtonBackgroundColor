3daa0692caf6568f22b516c6b11b2938f6cd2512
mattdipasquale <liveloveprosper@gmail.com>
2015-10-26 18:21:37 -0400

    Heed nil background color; support all scales

--- UIButtonBackgroundColor.swift
+++ UIButtonBackgroundColor.swift
@@ -2,15 +2,15 @@ import UIKit
 
 extension UIButton {
     func setBackgroundColor(color: UIColor?, forState state: UIControlState) {
-        if let color = color {
-            setBackgroundImage(UIImage.imageColored(color), forState: state)
-        }
+        let image = color != nil ? UIImage.imageColored(color!) : nil
+        setBackgroundImage(image, forState: state)
     }
 }
 
 extension UIImage {
     class func imageColored(color: UIColor) -> UIImage! {
-        let rect = CGRect(x: 0, y: 0, width: 0.5, height: 0.5)
+        let onePixel = 1 / UIScreen.mainScreen().scale
+        let rect = CGRect(x: 0, y: 0, width: onePixel, height: onePixel)
         UIGraphicsBeginImageContextWithOptions(rect.size, CGColorGetAlpha(color.CGColor) == 1, 0)
         let context = UIGraphicsGetCurrentContext()
         CGContextSetFillColorWithColor(context, color.CGColor)
